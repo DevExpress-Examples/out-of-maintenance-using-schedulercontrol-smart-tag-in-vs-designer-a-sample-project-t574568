c61866c043c659bb916b0c8c6bf9b6d3567d8fc6
DevExpressExampleBot <fakeEmail>
2022-12-17 16:34:04 +0400

    Source auto update [skip ci]

--- VB/SmartTagExample/CustomAppointmentWindow.xaml.vb
+++ VB/SmartTagExample/CustomAppointmentWindow.xaml.vb
@@ -1,27 +1,16 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
-Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Shapes
 Imports DevExpress.Xpf.Ribbon
 
 Namespace SmartTagExample
-	''' <summary>
-	''' Interaction logic for CustomAppointmentWindow.xaml
-	''' </summary>
-	Partial Public Class CustomAppointmentWindow
-		Inherits DXRibbonWindow
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+    ''' <summary>
+    ''' Interaction logic for CustomAppointmentWindow.xaml
+    ''' </summary>
+    Public Partial Class CustomAppointmentWindow
+        Inherits DXRibbonWindow
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 End Namespace
--- VB/SmartTagExample/CustomRecurrenceWindow.xaml.vb
+++ VB/SmartTagExample/CustomRecurrenceWindow.xaml.vb
@@ -1,27 +1,16 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
-Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Shapes
 Imports DevExpress.Xpf.Core
 
 Namespace SmartTagExample
-	''' <summary>
-	''' Interaction logic for CustomRecurrenceWindow.xaml
-	''' </summary>	
-	Partial Public Class CustomRecurrenceWindow
-		Inherits DXDialogWindow
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+    ''' <summary>
+    ''' Interaction logic for CustomRecurrenceWindow.xaml
+    ''' </summary>	
+    Public Partial Class CustomRecurrenceWindow
+        Inherits DXDialogWindow
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 End Namespace
